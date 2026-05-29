ef5ad032bd60d52a219f5cb29093b19da8b0b139
Harsha Vardhan Balanagu <bhv@meta.com>
2026-05-29 14:21:36 +0100

    fix for mobile upload

---
+++ src/polyfills.js
@@ -0,0 +1,16 @@
+// iOS Safari doesn't support ReadableStream async iteration (Symbol.asyncIterator),
+// which pdfjs-dist v5+ requires internally.
+if (typeof ReadableStream !== 'undefined' && !ReadableStream.prototype[Symbol.asyncIterator]) {
+  ReadableStream.prototype[Symbol.asyncIterator] = async function* () {
+    const reader = this.getReader();
+    try {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) return;
+        yield value;
+      }
+    } finally {
+      reader.releaseLock();
+    }
+  };
+}
